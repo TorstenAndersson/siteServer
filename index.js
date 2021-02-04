9e2b56b36f4dcd503f55768dbbd0e30819be4d98
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-04 23:07:50 +0100

    tests path and extension management

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const path = require('path');
 const fetch = require('node-fetch');
 const port = process.env.PORT;
 express()
-  .get("/*", (req, res) => {
+  /*.get("/*", (req, res) => {
     if (req.path != "johanFavicon.png") res.sendFile(req.path, {root: path.join(__dirname, "accessible")});
-  })
+  })*/
+  .get("/testing.html", (req, res) => res.send("HEJ"))
 
   .listen(port);
