e399341d5bb0158ecec1b0512ad1f8eeff625a2c
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-04 22:33:31 +0100

    trying loading local files through sendFile with path relativity

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path'); 
 const fetch = require('node-fetch');
 const port = process.env.PORT;
 express()
-  .get("/", (req, res) => res.sendFile("./index.html"))
+  .get("/", (req, res) => res.sendFile("./index.html", {root: path.join(__dirname)}))
 
   .listen(port);
