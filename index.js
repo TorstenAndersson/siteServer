22abda69e1d6a8d2d14f05f658c22e6f12faf21c
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-04 22:23:32 +0100

    trying loading local files through sendFile

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const cors = require("cors");
 const fetch = require('node-fetch');
 const port = process.env.PORT;
 express()
-  .get("/", (req, res) => res.sendFile("index.html"))
+  .get("/", (req, res) => res.sendFile("./index.html"))
 
   .listen(port);
