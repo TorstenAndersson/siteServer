d46dfff5d6b12222b1ae7711d4dd6fcb2dd560b8
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-04 22:21:40 +0100

    trying loading local files through require

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const fetch = require('node-fetch');
-const indexFile = require('./index.html')
 const port = process.env.PORT;
 express()
-  .use(cors({options: "https://xenodochial-varahamihira-6786dc.netlify.app/"}))
-
-  .get("/", (req, res) => res.send(indexFile))
+  .get("/", (req, res) => res.sendFile("index.html"))
 
   .listen(port);
