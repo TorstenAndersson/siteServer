bd9c768fe92c0834f3d4d569606d9f99e403bfb8
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-04 22:58:34 +0100

    tests file management

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const fetch = require('node-fetch');
 const port = process.env.PORT;
 express()
-  .get("/*", (req, res) => res.sendFile(req.path, {root: path.join(__dirname, "accessible")}))
+  .get("/*", (req, res) => {
+    if (req.path != "johanFavicon.png") res.sendFile(req.path, {root: path.join(__dirname, "accessible")});
+  })
 
   .listen(port);
