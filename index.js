79c774b70584dd1f46022eb318de732e54935673
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-05 17:18:38 +0100

    changed file reading to now read directory accessible

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const fs = require('fs');
 const parse = require("node-html-parser");
 const fetch = require("node-fetch");
 const port = process.env.PORT;
-
+/*
 var files;
 fs.readdirSync("./accessible/").forEach(file => {
   //const file = parse();
   files.append(file);
 });
+*/
 
 express()
   .get("/*", (req, res) => {
-    res.send(files);
+    //res.send(files);
+    res.send("");
   })
 
   .listen(port);
