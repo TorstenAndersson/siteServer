6b8c71febac58f369f56ff5908a752a81b370640
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-05 22:22:46 +0100

    debugging non existant file error

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const parser = require("node-html-parser");
 const fetch = require("node-fetch");
 const port = process.env.PORT;
 
+/*
 var files = "";
 fs.readdirSync("./accessible/").forEach(file => {
   if (file.endsWith(".html")) {
     const parsedContent = parser.parse(fs.readFileSync("./" + file, "utf-8"));
     parsedContent.querySelector("h1").innerHTML = "NEJ JAG HEJAR PÅ ER";
-    eval("var " + file.slice(0, -5).replace("-", "_") + '= "Hey"'); // + parsedContent.toString().replace("\n", "") + '";')
+    eval("var " + file.slice(0, -5).replace("-", "_") + ' = "Hey"'); // + parsedContent.toString().replace("\n", "") + '";')
   }
   //files.append(file);
 });
-
-/*
-files = fs.readdirSync("./accessible/");
-files = fs.readFileSync("./" + files[0], "utf-8");
 */
+
+var files = fs.readdirSync("./accessible/");
+//files = fs.readFileSync("./" + files[0], "utf-8");
 
 express()
   .get("/*", (req, res) => {
-    res.send(index.js);
+    res.send(files);
     //res.send("I DO WORK I SWEAR");
   })
 
