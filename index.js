315cfaa48cc7e5a2f095555b1df46242324fcfe2
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-05 17:06:46 +0100

    tests readign files

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const path = require('path'); 
+const fs = require('fs');
+import { parse } from 'node-html-parser';
 const fetch = require('node-fetch');
 const port = process.env.PORT;
+
+var files;
+fs.readdirSync("./accessible/merchandise").forEach(file => {
+  //const file = parse();
+  files.append(file);
+});
+
 express()
   .get("/*", (req, res) => {
-    res.sendFile(req.path, {root: path.join(__dirname, "accessible")});
+    res.send(files);
   })
 
   .listen(port);
