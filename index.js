0bb215f7d6659755983490f8613658cec804fa4f
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-02-05 17:22:38 +0100

    tests if it now works with propor app name

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const fs = require('fs');
 const parse = require("node-html-parser");
 const fetch = require("node-fetch");
 const port = process.env.PORT;
-/*
+
 var files;
 fs.readdirSync("./accessible/").forEach(file => {
   //const file = parse();
   files.append(file);
 });
-*/
 
 express()
   .get("/*", (req, res) => {
-    //res.send(files);
-    res.send("I DO WORK I SWEAR");
+    res.send(files);
+    //res.send("I DO WORK I SWEAR");
   })
 
   .listen(port);
